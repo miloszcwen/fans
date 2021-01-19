5f1a4f2c05031d6ab8a4a6992f6375cc8c3bc3be
miloszcwen <cwen.milosz@gmail.com>
2021-01-19 23:56:46 +0100

    add image placeholder (for loading avatar image)

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll =(props)=>{
     return (
-        <div style={{ overflowX: 'scroll', height: '800px'}}>
+        <div style={{ overflowX: 'scroll', height: '100vh'}}>
             {props.children}
         </div>
     );
