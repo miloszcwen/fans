86e93c7b44e51d2fc3d78b2e239cabe65e104a8a
miloszcwen <cwen.milosz@gmail.com>
2021-02-03 18:11:38 +0100

    modify scroll

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll =(props)=>{
     return (
-        <div style={{ overflowX: 'scroll', height: '100vh'}}>
+        <div style={{ overflowX: 'none', overflowY: 'none', height: 'auto'}}>
             {props.children}
         </div>
     );
